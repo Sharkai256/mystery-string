228b968a1928989844c9135e5966d03d7e6c77b5
Sharkai <justasharky@i.ua>
2026-01-14 15:26:51 +0200

    Added session and cookie.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,15 @@
 'use client'
 export default function Page() {
-	const user = {
-		name: "default",
-		role: "user"
+	const admin = {
+		name: "name"
 	}
-	// const user = {
-	// 	name: "ADMIN",
-	// 	role: "admin"
-	// }
 
 	return <>
 		<h1>this is an admin page.</h1>
 		<h1>
-			{user == null
+			{admin == null
 			? "could not load user info"
-			: `you are logged in as: ${user.name}`}
+			: `you are logged in as: ${admin.name}`}
 		</h1>
 	</>
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,73 +1,31 @@
-'use client'
-import { useRouter } from 'next/navigation';
-import { FormEvent } from "react";
+import { redirect } from "next/navigation";
+import { getSession, login, logout } from "@/lib";
 
-export default function loginPage() {
-	const router = useRouter()
-	// const [inputUser, setInputUser] = useState("")
-	// const [inputPass, setInputPass] = useState("")
-
-	// const reqBody = {
-	// 	userName: inputUser,
-	// 	userPassword: inputPass
-	// }
-
-	// const login = async () => {
-	// 	const data = await fetch("http://localhost:3000/admin/api", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			'Content-Type': 'application/json'
-	// 		},
-	// 		body: JSON.stringify(reqBody)
-	// 	}).then(res => res.json()).then()
-
-	// 	console.log(data)
-	// };
-
-	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault()
-
-		const formData = new FormData(event.currentTarget)
-		const email = formData.get('email')
-		const password = formData.get('password')
-
-		const response = await fetch('/login/api', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email, password }),
-		})
-
-		if (!response.ok) console.log("Error occured")
-
-		response.json().then(data => {
-			// if (data.authentificated) router.push("/admin")
-			console.log(data)
-		})
-	}
-
-	// return <>
-	// 	<input
-	// 		type="text"
-	// 		placeholder="username"
-	// 		maxLength={15}
-	// 		value={inputUser}
-	// 		onChange={(e) => setInputUser(e.target.value)}
-	// 	/>
-	// 	<input
-	// 		type="text"
-	// 		placeholder="password"
-	// 		maxLength={15}
-	// 		value={inputPass}
-	// 		onChange={(e) => setInputPass(e.target.value)}
-	// 	/>
-	// 	<button onClick={login}>login</button>
-	// </>
-
+export default async function Page() {
+	const session = await getSession();
 	return (
-		<form onSubmit={handleSubmit}>
-			<input type="text" name="email" placeholder="Email" required />
-			<input type="text" name="password" placeholder="Password" required />
-			<button type="submit">Login</button>
-		</form>
-	)
+		<>
+			<form
+				action={async (formData) => {
+					"use server";
+					await login(formData);
+					redirect("/login");
+				}}
+			>
+				<input type="email" placeholder="Email" />
+				<br />
+				<button type="submit">Login</button>
+			</form>
+			<form
+				action={async () => {
+					"use server";
+					await logout();
+					redirect("/login");
+				}}
+			>
+				<button type="submit">Logout</button>
+			</form>
+			<pre>{JSON.stringify(session, null, 2)}</pre>
+		</>
+	);
 }
